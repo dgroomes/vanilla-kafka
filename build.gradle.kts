66bf8342838393769df3a7b7f39ca76fb35d8475
David Groomes <dwgroomes@gmail.com>
2021-01-21 19:04:15 -0600

    Upgrade to Gradle 6.8 and add a "assembleAll" task

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+/**
+ * A convenience task to assemble all of the included projects. This is useful as a quick smoke test.
+ */
+tasks.register("assembleAll") {
+    dependsOn(gradle.includedBuild("interactive").task(":assemble"))
+    dependsOn(gradle.includedBuild("kafka-in-kafka-out").task(":app:assemble"))
+    dependsOn(gradle.includedBuild("kafka-in-kafka-out").task(":load-simulator:assemble"))
+    dependsOn(gradle.includedBuild("kafka-in-kafka-out").task(":test-harness:assemble"))
+    dependsOn(gradle.includedBuild("spring-errors").task(":assemble"))
+    dependsOn(gradle.includedBuild("spring-headers").task(":assemble"))
+    dependsOn(gradle.includedBuild("spring-seekable").task(":assemble"))
+    dependsOn(gradle.includedBuild("streams").task(":assemble"))
+}
